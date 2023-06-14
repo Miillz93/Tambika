1c7a5f26a82d40b77e9b67fff6021f79852b5909
miillz <romaric.miaka.sio@gmail.com>
2023-06-14 19:26:13 +0200

    dbcontext

--- NGallery/Data/ApplicationDbContext.cs
+++ NGallery/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace NGallery.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : IdentityDbContext<User>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
